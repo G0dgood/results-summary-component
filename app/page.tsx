d5298e85c3a23ee93a1e4bc17d7d5d0cab3bdfe3
Okoro Godwin Chinedu <50265356+G0dgood@users.noreply.github.com>
2026-03-19 11:38:13 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import data from "../data.json";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-white sm:bg-pale-blue">
+      <h1 className="sr-only">Results summary component</h1>
       <div className="flex flex-col sm:flex-row bg-white sm:rounded-[32px] sm:shadow-[20px_30px_60px_rgba(0,0,0,0.05)] max-w-[736px] w-full overflow-hidden">
         
         {/* Left Section: Result */}
